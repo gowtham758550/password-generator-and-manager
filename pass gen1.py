bd4ab53a56e55bcb5ff8d10bc5de5b3e234cbab7
gowtham758550 <63159252+gowtham758550@users.noreply.github.com>
2020-04-04 21:30:41 +0530

    Add files via upload

---
+++ pass gen1.py
@@ -0,0 +1,18 @@
+import string
+import random
+from datetime import datetime
+
+
+print("Enter number of characters for password,website name and id or username")
+
+def randompassword():
+    chars=string.ascii_uppercase + string.ascii_lowercase + string.digits+string.punctuation
+    return''.join(random.choice(chars) for x in range(0,int(input())))
+    
+    
+
+
+
+
+with open("/storage/emulated/0/password.txt","a") as file:
+	file.write("\n\n"+str(datetime.now().strftime("%B-%d-%y\t%H-%M-%S"))+"\nPassword : "+randompassword()+"\nWebsite : "+input()+"\nId : "+input())
