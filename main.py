b60ae5c1137ac57539b23ef67be50399f4526c3a
Gowtham <gowtham758550@gmail.com>
2020-08-30 08:57:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Password Generator and Manager version 2.1
+#Password Generator and Manager version 2.2
 
 # Copyright (C) Gowtham 2019-2020 <gowtham758550@gmail.com>
 # Copyright (C) 2019-2020 M.Anish <aneesh25861@gmail.com>
